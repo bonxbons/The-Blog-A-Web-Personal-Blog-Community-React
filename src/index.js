c9db6c1a92da7eff3d1204007f241fc267443ab1
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2024-04-16 23:56:14 -0500

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -9,19 +9,13 @@ import {
 
 import './style.css'
 import Blog from './views/blog'
-import Supportingtext from './views/supportingtext'
-import Header from './views/header'
-import Footer from './views/footer'
 import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={Blog} exact path="/blog" />
-        <Route component={Supportingtext} exact path="/supportingtext" />
-        <Route component={Header} exact path="/" />
-        <Route component={Footer} exact path="/footer" />
+        <Route component={Blog} exact path="/" />
         <Route component={NotFound} path="**" />
         <Redirect to="**" />
       </Switch>
